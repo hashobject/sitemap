a447d210234d22e678db45d682e8c9c609fc5eb1
Anton Podviaznikov <podviaznikov@gmail.com>
2013-09-19 22:59:46 -0300

    bump deps and version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject sitemap "0.2.0"
+(defproject sitemap "0.2.1"
   :description "Clojure library for sitemap generation."
-  :signing {:gpg-key "HashObject Ltd <team@hashobject.com>"}
+  :signing {:gpg-key "Hashobject Ltd <team@hashobject.com>"}
   :url "https://github.com/hashobject/sitemap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [hiccup "1.0.3"]])
+                 [hiccup "1.0.4"]])
