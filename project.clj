e571249fd2eedf9d3a814c87ea97b291510bd818
Anton Podviaznikov <podviaznikov@gmail.com>
2014-04-30 00:59:48 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject sitemap "0.2.1"
+(defproject sitemap "0.2.2"
   :description "Clojure library for sitemap generation."
   :signing {:gpg-key "Hashobject Ltd <team@hashobject.com>"}
   :url "https://github.com/hashobject/sitemap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [hiccup "1.0.4"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [hiccup "1.0.5"]])
