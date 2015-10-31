d3bfa0fd8980c06d9487eb9e381b49fbd6c40dc3
Anton Podviaznikov <podviaznikov@gmail.com>
2015-10-30 19:50:17 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :url "https://github.com/hashobject/sitemap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [hiccup "1.0.5"]])
