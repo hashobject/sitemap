9cde15e653532b4b736b8061a90ba0dc54c6c4de
Ben Cook <b@bencook.ca>
2017-10-16 02:14:25 -0700

    Bump version to 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sitemap "0.2.5"
+(defproject sitemap "0.3.0"
   :description "Clojure library for sitemap generation."
   :signing {:gpg-key "Hashobject Ltd <team@hashobject.com>"}
   :url "https://github.com/hashobject/sitemap"
