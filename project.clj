f3320e71563bb9007899007142cb2fd674bca9a6
podviaznikov <anton@podviaznikov.com>
2018-11-06 16:57:38 -0800

    do not sign release

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :url "https://github.com/hashobject/sitemap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/hashobject/sitemap"}]
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
                         ["snapshots" {:sign-releases false :url "https://clojars.org/hashobject/sitemap"}]]            
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [hiccup "1.0.5"]])
